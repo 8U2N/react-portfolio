030b432a7ed5a6edd201341297a1b4d8c7f34359
8U2N <tylerburnworthwriting@gmail.com>
2022-04-26 10:34:49 -0700

    Added NavLink Routes

--- src/components/app.js
+++ src/components/app.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
 import moment from "moment";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import PortfolioContainer from './portfolio/portofolio-container';
 import NavigationContainer from './navigation/navigation-container';
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Blog from "./pages/blog";
 //  , { BioTextNode }
 
 export default class App extends Component {
   render() {
     return (
       <div className="app">
-        <div>
-          <NavigationContainer />
-        </div>
+        <Router>
+          <div>
+            <NavigationContainer />
+
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about-me" component={About} />
+              <Route exact path="/contact" component={Contact} />
+              <Route exact path="/blog" component={Blog} />
+            </Switch>
+          </div>
+        </Router>
         <div>
           <img src="/assets/images/developer-photo.jpg" alt="profile-photo" width="400"></img>
         </div>
@@ -20,10 +34,8 @@ export default class App extends Component {
           <h2>Junior Full Stack Developer</h2>
           <h3>&#128368; {moment().format('MMMM Do YYYY, h:mm:ss a')}</h3>
         </div>
-        <div>
         <PortfolioContainer />
         {/* <BioTextNode /> */}
-        </div>
       </div>
     );
   }
